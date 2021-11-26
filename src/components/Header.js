00f89932372a8c904d975f6e534a5850a9610385
thevindusenanayake <thevindusenanayake76@gmail.com>
2021-11-26 16:22:38 +0530

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,32 @@ import styled from 'styled-components';
 function Header() {
 	return (
 		<Container>
-			<Logo src="" />
+			<Logo src="/images/logo.svg" alt="logo"/>
 			<Navbar>
-				g
+				<a>
+					<img src="/images/home-icon.svg" alt="home" />
+					<span>Home</span>
+				</a>
+				<a>
+					<img src="/images/search-icon.svg" alt="home" />
+					<span>search</span>
+				</a>
+				<a>
+					<img src="/images/watchlist-icon.svg" alt="home" />
+					<span>watchlist</span>
+				</a>
+				<a>
+					<img src="/images/original-icon.svg" alt="home" />
+					<span>originals</span>
+				</a>
+				<a>
+					<img src="/images/movie-icon.svg" alt="home" />
+					<span>movies</span>
+				</a>
+				<a>
+					<img src="/images/series-icon.svg" alt="home" />
+					<span>series</span>
+				</a>
 			</Navbar>
 		</Container>
 	)
@@ -17,6 +40,9 @@ export default Header
 const Container = styled.div`
 	height: 70px;
 	background: #090b13;
+	display: flex;
+	align-items: center;
+	padding: 0 36px;
 
 `
 
@@ -25,5 +51,21 @@ const Logo = styled.img`
 `
 
 const Navbar = styled.div`
+	display: flex;
 
+	a {
+		display: flex;
+		align-items: center;
+		padding: 0 12px;
+
+		img {
+			height: 20px;
+		}
+
+		span {
+			font-size: 13px;
+			letter-spacing: 1.42px;
+			text-transform: uppercase;
+		}
+	}
 `
