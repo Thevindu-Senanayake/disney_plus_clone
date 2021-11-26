a3e87cb2ad45b69f508ae2330be60cbd65908da4
thevindusenanayake <thevindusenanayake76@gmail.com>
2021-11-26 15:14:14 +0530

    add images and main components

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+	return (
+		<Container>
+			<Logo src="" />
+			<Navbar>
+				g
+			</Navbar>
+		</Container>
+	)
+}
+
+export default Header
+
+const Container = styled.div`
+	height: 70px;
+	background: #090b13;
+
+`
+
+const Logo = styled.img`
+	width: 80px;
+`
+
+const Navbar = styled.div`
+
+`
